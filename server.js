449ee28724e9894fbbea2843746f055560442c6a
Nguyễn Phước Quang <hungdt@runsystem.net>
2015-10-12 17:41:13 +0700

    change app to photo share

--- server.js
+++ server.js
@@ -1,42 +1,99 @@
-/*
+var fs      = require("fs");
+var express = require("express");
+var app     = express();
+var server  = require("http").Server(app);
+var io      = require("socket.io").listen(server);
+var url     = require("url");
+var qr      = require('qr-image');
 
-  NodeGame: Shooter
-  Copyright (c) 2010 Ivo Wetzel.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false });
+app.use(express.static(__dirname + '/public'));
+server.listen(80);
+console.log("Server - Waiting connection at port: 80");
 
-  All rights reserved.
-
-  NodeGame: Shooter is free software: you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  NodeGame: Shooter is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License along with
-  NodeGame: Game. If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-
-// Server ----------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-var NodeGame = require('./nodegame/nodegame');
-
-// Setup Game Model
-Shooter = NodeGame.Model(20);
-require('./shooter/game');
-require('./shooter/client');
-require('./shooter/actors');
-
-// Start a Server
-Shooter.Server({
-    'port': 3000,
-    'status': false,
-    'recordFile': './../record[date].js',
-    'record': false,
-    'flash': false
+app.get("/qrcode", function(req, res){
+  var text = url.parse(req.url, true).query.text;
+  try {
+      var img = qr.image(text);
+      res.writeHead(200, {'Content-Type': 'image/png'});
+      img.pipe(res);
+  } catch (e) {
+      res.writeHead(414, {'Content-Type': 'text/html'});
+      res.end('<h1>414 Request-URI Too Large</h1>');
+  }
 });
 
+app.get("/", function(req, res){
+  res.render('home.jade');
+});
+app.get("/send", function(req, res){
+  var toID = url.parse(req.url, true).query.to;  
+  res.render('sender.jade', {sendToID: toID});
+});
+
+var bindingCodes = {};
+io.sockets.on('connection', function (socket) { 
+  var simpleCode = getSimpleCode();
+  bindingCodes["c"+simpleCode] = socket.id,
+  socket.emit("greeting", {
+    message: "Welcome to connect server.",
+    session: socket.id, 
+    simpleCode: simpleCode
+  });
+
+  socket.on("sendPhoto", function(data){
+    var guess = data.base64.match(/^data:image\/(png|jpeg);base64,/)[1];
+    var ext = "";
+    switch(guess) {
+      case "png"  : ext = ".png"; break;
+      case "jpeg" : ext = ".jpg"; break;
+      default     : ext = ".bin"; break;
+    }
+    var savedFilename = "/upload/"+randomString(10)+ext;
+    fs.writeFile(__dirname+"/public"+savedFilename, getBase64Image(data.base64), 'base64', function(err) {
+      if (err !== null)
+        console.log(err);
+      else {
+        var sendToID = bindingCodes["c"+data.sendToID];
+        console.log("Send to :"+data.sendToID);
+        io.to(sendToID).emit("receivePhoto", {
+          path: savedFilename,
+        });
+        console.log("Send photo success!");        
+      }
+    });
+  });
+
+  socket.on('disconnect', function() {
+    for(var code in bindingCodes) {
+      if (bindingCodes[code] == socket.id) 
+        delete bindingCodes[code];
+    }
+    console.log("Client #" + socket.id + " is disconnect.");
+  }); 
+});
+
+function randomString(length)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+function getBase64Image(imgData) {
+    return imgData.replace(/^data:image\/(png|jpeg|jpg);base64,/, "");
+}
+function randomInt(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+function getSimpleCode(){
+  var code = randomInt(10000, 99999);
+  if (typeof bindingCodes["c"+code] != "undefined")
+    return getSimpleCode();
+  return code;
+}
